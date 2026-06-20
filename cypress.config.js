c773c6be057c59616dfa827915645794128bf367
Petar Jovanovic <jovanovicpetar92@gmail.com>
2026-06-20 08:59:18 +0200

    Configure Mochawesome reporting

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,15 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  reporter: "mochawesome",
+
+  reporterOptions: {
+    reportDir: "reports",
+    overwrite: false,
+    html: true,
+    json: true,
+  },
+
   allowCypressEnv: false,
 
   e2e: {
